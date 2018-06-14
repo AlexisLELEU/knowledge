c9dd2407982b99d20a929eb9fbd034dd45f9a6b3
AlexisLELEU <alexis.leleu@hetic.net>
2018-06-14 14:04:52 +0200

    header isolation and include

--- index.php
+++ index.php
@@ -17,26 +17,9 @@ $driver = new DriverPrestashop();
             <meta charset="UTF-8">
         </head>
         <body>
-            <header class="header">
-                <div class='header-content'>
-                    <div class='header-content__menu'>
-                        <img class='header-content_logo' src="./images/knowledge.svg" alt="">
-                        <nav class="header__nav">
-                            <ul class="header-nav__ul">
-                                <li class="header-nav__li">Accueil</li>
-                                <li class="header-nav__li">Tickets</li>
-                                <li class="header-nav__li">Clients</li>
-                            </ul>
-                        </nav>
-                        </div>
-                    <div class='header__select'>
-                        <div class='header-select__custom'>
-                            <p>Sptam</p>
-                            <a href="./connect_file/logOut.php"><img src="./images/Shape.svg" alt=""></a>
-                        </div>
-                    </div>
-                </div>
-            </header>
+            <?php
+                require_once 'includes/header.php';
+            ?>
             <main>
                 <div class='search-container'>
                     <div class='search-container__search'>
